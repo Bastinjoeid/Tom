04853971de05c858ed7008b933660a2f541c8f71
Bastinjoeid <102047556+Bastinjoeid@users.noreply.github.com>
2022-09-18 17:21:59 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -272,11 +272,11 @@ async def cb_handler(client: Client, query: CallbackQuery):
         elif query.data == "about":
             buttons = [
                 [
-                    InlineKeyboardButton('Owner', url='https://t.me/Ramanan_TG'),
-                    InlineKeyboardButton('Group', url='https://t.me/Cinema_Beacon_Group')
+                    InlineKeyboardButton('Owner', url='https://t.me/sovel_jaison'),
+                    InlineKeyboardButton('Group', url='https://t.me/CINEMA_HUB_GROUP')
                 ]
                 ]
-            await query.message.edit(text="Language : <code>Python3</code>\nLibrary : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio</a>\nOwner : <a href='https://t.me/Ramanan_TG'>Ramanan</a>\nGroup : <a href='https://t.me/Cinema_Beacon_Group'>📀 ᴄɪɴᴇᴍᴀ ʙᴇᴀᴄᴏɴ 📀</a> </b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
+            await query.message.edit(text="Language : <code>Python3</code>\nLibrary : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio</a>\nOwner : <a href='https://t.me/sovel_jaison'>SOVEL JAISON</a>\nGroup : <a href='https://t.me/CINEMA_HUB_GROUP'>📀 Cinema Hub 📀</a> </b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
 
 
 
@@ -297,8 +297,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     f_caption = f"{files.file_name}"
                 buttons = [
                     [
-                        InlineKeyboardButton('Owner', url='https://t.me/Ramanan_TG'),
-                        InlineKeyboardButton('Group', url='https://t.me/Cinema_Beacon_Group')
+                        InlineKeyboardButton('Owner', url='https://t.me/sovel_jaison'),
+                        InlineKeyboardButton('Group', url='https://t.me/CINEMA_HUB_GROUP')
                     ]
                     ]
                 
